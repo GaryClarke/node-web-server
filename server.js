7a4ee1c71cdc5e4e636b71199602cd49de20279e
Gary Clarke <garyclarke1974@gmail.com>
2018-10-11 16:25:24 +0100

    Lecture 42

--- server.js
+++ server.js
@@ -4,7 +4,25 @@ var app = express();
 
 app.get('/', (req, res) => {
 
-    res.send('Hello express!');
+    // res.send('<h1>Working!</h1>');
+    res.send({
+        name: 'Gary',
+        likes: [
+            'This', 'That', 'Other',
+        ]
+    })
+});
+
+app.get('/about', (req, res) => {
+
+    res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+
+    res.send({
+        errorMessage: 'Bad shit happened'
+    });
 });
 
 app.listen(3000);
