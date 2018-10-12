0cd24fa99222de0a900009c7f186cb4f071ca488
Gary Clarke <garyclarke1974@gmail.com>
2018-10-12 14:50:45 +0100

    Lecture 44

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
 
-    // res.send('<h1>Working!</h1>');
-    res.send({
-        name: 'Gary',
-        likes: [
-            'This', 'That', 'Other',
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Welcome',
+        welcomeMessage: 'Welcome to our page',
+        currentYear: new Date().getFullYear()
     })
 });
 
 app.get('/about', (req, res) => {
 
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) => {
@@ -25,4 +30,7 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+
+    console.log('Server is up on port 3000')
+});
