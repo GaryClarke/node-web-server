7fa17e0f2f183e34cb7417a69d5cb99ca54a62c9
Gary Clarke <garyclarke1974@gmail.com>
2018-10-15 11:57:12 +0100

    Lecture 46

--- server.js
+++ server.js
@@ -1,17 +1,52 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+
+    var now = new Date().toString();
+
+    var logString = `${now}: ${req.method} ${req.url}`;
+
+    console.log(logString);
+
+    fs.appendFile('server.log', logString  + '\n', (err) => {
+
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+
+    next();
+});
+
+// app.use((req, res, next) => {
+//
+//     res.render('maintenance.hbs', {})
+// });
+
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
 
     res.render('home.hbs', {
         pageTitle: 'Welcome',
         welcomeMessage: 'Welcome to our page',
-        currentYear: new Date().getFullYear()
     })
 });
 
@@ -19,7 +54,6 @@ app.get('/about', (req, res) => {
 
     res.render('about.hbs', {
         pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
     });
 });
 
